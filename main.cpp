9c66322585194d157652e00d88ca057e84816eff
def1127 <4934208+def1127@users.noreply.github.com>
2023-01-31 20:04:07 -0500

    finishes incorrect location.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 #include "ClientFeedback.h"
 #include "SmartClient.h"
 #include "LockBox.h"
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 int main()
 {
+
+    int digits, range;
+
+    cout << "Enter number of digits in combination: ";
+    cin >> digits;
+
+    cout << "\nEnter range of digits: ";
+    cin >> range;
+
+    cout << "\nGenerating combination...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    lockBox secret(digits, range);
 
     return 0;
 
