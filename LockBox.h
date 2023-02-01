e90e4488f14a12ee8c234aa094d4fb0c29f9f17b
def1127 <4934208+def1127@users.noreply.github.com>
2023-01-31 19:43:56 -0500

    I DONT KNOW

--- LockBox.h
+++ LockBox.h
@@ -8,8 +8,10 @@ class lockBox
     int incorrectLocation(lockBox input);
     lockBox();
     lockBox(int n, int m);
+    lockBox(std::vector<int> input);
 
     private:
     std::vector<int> combination;
+    std::vector<int> generateCombination(int n, int m);
 
 };
