2ba9140f30d42b8553998cd4c97eef391332cfdd
Julian <crowley.jul@northeastern.edu>
2023-02-08 21:43:15 -0500

    updated documentation for LockBox Files

--- LockBox.cpp
+++ LockBox.cpp
@@ -13,6 +13,8 @@ lockBox::lockBox()
 }
 
 /* Constructs a lockbox object with a combination of n length (with each digit between 0 and m).*/
+/* @param n is the number of digits in combination. */
+/* @param m is the upper limit that each digit can be. */
 lockBox::lockBox(int n, int m)
 {
 
@@ -21,6 +23,7 @@ lockBox::lockBox(int n, int m)
 }
 
 /* Construct a lockbox object with combination that matches int vector. */
+/* @param input is the combination for lockbox to store. */
 lockBox::lockBox(std::vector<int> input)
 {
 
@@ -29,6 +32,10 @@ lockBox::lockBox(std::vector<int> input)
 }
 
 /* Generates a combination of random digits (int vector) of length n with each digit in range 0 - m. */
+/* @param n is the length of the combination. */
+/* @param m is the upper limit that each digit can be. */
+/* @return a int vector of length n with each digit being randomly between 0 and m. */
+    std::vector<int> generateCombination(int n, int m);
 std::vector<int> lockBox::generateCombination(int n, int m)
 {
 
@@ -47,6 +54,8 @@ std::vector<int> lockBox::generateCombination(int n, int m)
 } // closes combination generator function
 
 /* Returns the number of digits (int) where the numbers and positions match in input lockbox and in object. */
+/* @param input is the input lockbox which contains an input combination for comparision */
+/* @return returns the number of positions where the combination of this lockbox and the input are the same. */
 int lockBox::correctLocation(lockBox input)
 {
 
@@ -68,6 +77,7 @@ int lockBox::correctLocation(lockBox input)
 } // closes correct location function
 
 /* Prints out the lockboc combination in console.*/
+/* @return the combination stored in lockbox as a int vector. */
 std::vector<int> lockBox::printCombination()
 {
     return combination;
@@ -75,6 +85,8 @@ std::vector<int> lockBox::printCombination()
 }
 
 /* Returns the number of singular ints (int) that are in input lockbox that are also in object in the incorrect location. */
+/* @param input is the input lockbox which contains an input combination for comparision */
+/* @return returns the number of digits in the input that are in the lockbock combo but not in the correct location. */
 int lockBox::incorrectLocation(lockBox input)
 {
 
--- LockBox.h
+++ LockBox.h
@@ -8,22 +8,33 @@ class lockBox
 
 public:
     /* Returns the number of digits (int) where the numbers and positions match in input lockbox and in object. */
+    /* @param input is the input lockbox which contains an input combination for comparision */
+    /* @return returns the number of positions where the combination of this lockbox and the input are the same. */
     int correctLocation(lockBox input);
     /* Returns the number of singular ints (int) that are in input lockbox that are also in object in the incorrect location. */
+    /* @param input is the input lockbox which contains an input combination for comparision */
+    /* @return returns the number of digits in the input that are in the lockbock combo but not in the correct location. */
     int incorrectLocation(lockBox input);
     /* Constructs a lockbox object with a combination of length three (with each digit between 0 and 9).*/
     lockBox();
     /* Constructs a lockbox object with a combination of n length (with each digit between 0 and m).*/
+    /* @param n is the number of digits in combination. */
+    /* @param m is the upper limit that each digit can be. */
     lockBox(int n, int m);
     /* Construct a lockbox object with combination that matches int vector. */
+    /* @param input is the combination for lockbox to store. */
     lockBox(std::vector<int> input);
     /* Prints out the lockboc combination in console.*/
+    /* @return the combination stored in lockbox as a int vector. */
     std::vector<int> printCombination();
 
 private:
     /* Combination stored as private int vector. */
     std::vector<int> combination;
     /* Generates a combination of random digits (int vector) of length n with each digit in range 0 - m. */
+    /* @param n is the length of the combination. */
+    /* @param m is the upper limit that each digit can be. */
+    /* @return a int vector of length n with each digit being randomly between 0 and m. */
     std::vector<int> generateCombination(int n, int m);
 
 };
